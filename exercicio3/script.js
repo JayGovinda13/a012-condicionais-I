63c30412e53e85f0583e311c66e5099b00697d72
Juliano Amaral <jaygovinda13@gmail.com>
2023-02-14 11:01:06 -0300

    revisão condicionais I feita

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,21 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt(`Digite aqui sua nacionalidade`).toLowerCase()
+
+// - brasileira;
+// - argentina;
+// - uruguaia;
+// - chilena;
+// - colombiana;
+
+if (nacionalidade === `brasileira`) {
+    console.log(`Você é do Brasil`)
+} else if (nacionalidade === `argentina`) {
+    console.log(`Você é da Argentina`)
+} else if (nacionalidade === `chilena`) { 
+    console.log(`Você é do Chile`)
+} else if (nacionalidade === `uruguaia`) {
+    console.log (`Você é do Uruguai`)
+} else if (nacionalidade === `colombiana`) {
+    console.log (`Você é da Colombia`)
+} else {
+    console.log (`Nacionalidade não encontrada`)
+}
